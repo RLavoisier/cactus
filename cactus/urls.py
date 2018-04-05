ee80b9e43743f3c9f43a11032f1dd1c3cac8a7ff
Renaud <renaud.lavoisier@gmail.com>
2018-04-05 19:21:54 +0200

    Correction login view supprimée

--- cactus/urls.py
+++ cactus/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from saisiecontrat.views import login, creationcontrat, create_entreprise, create_alternant, inform_contrat, inform_mission
+from saisiecontrat.views import creationcontrat, create_entreprise, create_alternant, inform_contrat, inform_mission
 from django.urls import path, include
 
 from cactus.views import accueil
