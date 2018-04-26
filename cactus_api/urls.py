c636fa0328106768e074641531b025868baae3ca
Renaud <renaud.lavoisier@gmail.com>
2018-04-26 02:35:22 +0200

    Calcul des rémunération

--- cactus_api/urls.py
+++ cactus_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from cactus_api.views import ValiderDateDebutContrat
+from cactus_api.views import ValiderDateDebutContrat, RecupererAnneesFormations, ValiderDateFinContrat
 
 urlpatterns = [
     path("valider_date_debut_contrat/", ValiderDateDebutContrat.as_view(), name="valider_date_debut_contrat"),
+    path("valider_date_fin_contrat/", ValiderDateFinContrat.as_view(), name="valider_date_fin_contrat"),
+    path("recuperer_remuneration/", RecupererAnneesFormations.as_view(), name="recuperer_remuneration"),
 ]
