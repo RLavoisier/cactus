1e29c8b1ae9ad1cfd430cde46c094f13c1d9ba73
Renaud <renaud.lavoisier@gmail.com>
2018-07-25 13:02:35 +0200

    Correction du problème d'envoi d'email de changement de mot de passe

--- cactus/settings/dev.py
+++ cactus/settings/dev.py
@@ -1,20 +1,7 @@
 from .base import *
 
-# A utiliser en mode test
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.cfa-epure.com'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = 'no_reply@cfa-epure.com'
-EMAIL_HOST_PASSWORD = 'imRp4%04'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_TIMEOUT = 30
-EMAIL_SSL_KEYFILE = None
-EMAIL_SSL_CERTFILE = None
-
+"""
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -26,5 +13,9 @@ EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 30
 EMAIL_SSL_KEYFILE = None
 EMAIL_SSL_CERTFILE = None
-
+"""
 DEBUG = True
+
+# A utiliser en mode test
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_MAIL = "test@test.fr"
